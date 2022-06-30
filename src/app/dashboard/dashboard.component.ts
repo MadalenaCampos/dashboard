72b2a662ea868746c9335463c34578c6e5b79457
MadalenaCampos <madacampos02@gmail.com>
2022-06-30 16:09:49 -0300

    :sparkles: implementado grafico de rosca

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 
+// Highcharts
+import * as Highcharts from 'highcharts';
+import { DadosService } from './service';
+
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
+// Highcharts
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
 })
 export class DashboardComponent implements OnInit {
+  public generosMaisOuvidos!: any;
 
-  constructor() { }
+  constructor(private dadosService: DadosService) {}
 
-  ngOnInit(): void {
+  public carregarGraficoDeRosca() {
+    this.dadosService.getDados().subscribe((data) => {
+      this.generosMaisOuvidos = {
+        chart: {
+          plotBorderWidth: 0,
+          plotShadow: false,
+          type: 'pie',
+        },
+        credits: {
+          enabled: false,
+        },
+        title: {
+          text: '',
+        },
+        legend: {
+          align: 'right',
+          verticalAlign: 'middle',
+        },
+        tooltip: {
+          pointFormat: '<b>{point.percentage:.2f}%</b>',
+        },
+        accessibility: {
+          point: {
+            valueSuffix: '%',
+          },
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: false,
+            },
+            showInLegend: true,
+          },
+        },
+        series: [
+          {
+            innerSize: '50%',
+            data: data,
+          },
+        ],
+      };
+    });
+
+    Highcharts.chart('graficoDeRosca', this.generosMaisOuvidos);
   }
 
+  ngOnInit() {
+    this.carregarGraficoDeRosca();
+  }
 }
